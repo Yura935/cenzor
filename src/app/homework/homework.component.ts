309d118e11ea90b2324a5a55ad92bb79025f4f68
Yura935 <boyko.yuriy16@gmail.com>
2020-11-19 18:32:28 +0200

    init

---
+++ src/app/homework/homework.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homework',
+  templateUrl: './homework.component.html',
+  styleUrls: ['./homework.component.css']
+})
+export class HomeworkComponent implements OnInit {
+  word = '';
+  text = '';
+  badWords = '';
+  wordBorderColor = 'gray';
+  textBorderColor = 'gray';
+  inputPlaceholder = 'word here...';
+  areaPlaceholder = 'text here...';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addWord(): void {
+    if (this.word.length > 0) {
+      if (this.badWords.length == 0) {
+        this.badWords += `${this.word}`;
+      }
+      else {
+        this.badWords += `, ${this.word}`;
+      }
+      this.word = '';
+      this.wordBorderColor = 'gray';
+      this.inputPlaceholder = 'word here...'
+    }
+    else {
+      this.wordBorderColor = 'red';
+      this.inputPlaceholder = 'Please write a word!'
+    }
+  }
+  reset(): void {
+    this.badWords = '';
+  }
+  cenzor(): void {
+    if (this.text.length > 0) {
+      let str = this.text.split(' ');
+      let words = this.badWords.split(', ');
+      for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < words.length; j++) {
+          if (str[i] == words[j]) {
+            let length = str[i].length
+            str[i] = '';
+            for (let k = 0; k < length; k++) {
+              str[i] += '*';
+            }
+          }
+          else {
+            str[i] = str[i];
+          }
+        }
+        this.text = '';
+        str.forEach(element => {
+          this.text += `${element} `;
+        });
+
+        this.textBorderColor = 'green';
+        this.areaPlaceholder = 'text here...'
+      }
+    }
+    else {
+      this.text = '';
+      this.textBorderColor = 'red';
+      this.areaPlaceholder = 'Please write a word!'
+    }
+  }
+}
